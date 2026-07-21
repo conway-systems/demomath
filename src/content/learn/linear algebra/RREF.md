ffc7c77b68fae191fcf7daaa0ef53471433feb5c
Ruan Kellard <ruankellard@gmail.com>
2026-07-21 19:22:38 +0100

    add searching

---
+++ src/content/learn/linear algebra/RREF.md
@@ -0,0 +1,6 @@
+---
+title: RREF (reduced row ecchelon form)
+path: linear-algebra/RREF
+---
+
+$$\begin{bmatrix} 1 & 2 \\ 3 & 4 \end{bmatrix}$$
--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,3 +1,6 @@
+import Levenshtein from 'fast-levenshtein';
+import jaroWinkler from 'jaro-winkler';
+
 export interface NavNode {
     name: string;
     slug?: string;
@@ -40,7 +43,7 @@ export function buildNavTree(entries: { id: string; data: { title: string, path:
     return root;
 }
 
-export function buildSearchTree(root: NavNode[]) : SearchNode[] {
+export function buildSearchList(root: NavNode[]) : SearchNode[] {
     let searchNodes: SearchNode[] = [];
     let stack: NavNode[] = [...root];
 
@@ -57,4 +60,51 @@ export function buildSearchTree(root: NavNode[]) : SearchNode[] {
     }
 
     return searchNodes;
+}
+
+function normalisedLevenshtein(v1: string, v2: string) : number {
+    return Levenshtein.get(v1, v2) / Math.max(v1.length, v2.length);
+}
+
+function stringDistance(query: string, comparison: string) : number {
+    let lev = normalisedLevenshtein(query, comparison);
+    if (comparison.startsWith(query)) lev /= 2;
+    return lev;
+}
+
+export function searchDropdown(dropdown: HTMLElement, value: string, count: number) {
+    if (value.length === 0) {
+        dropdown.classList.add('search-closed');
+    } else {
+        dropdown.classList.remove('search-closed');
+    }
+
+    interface Entry {
+        el: Element;
+        d: number;
+    }
+
+    const value_lower = value.toLowerCase();
+
+    const entries: Entry[] = Array.from(dropdown.children).map((val) => {
+        const entryVal = val.children[0].innerHTML.split('</span>')[1].trim().toLowerCase();
+        const dist = stringDistance(value_lower, entryVal);
+
+        const ent: Entry = {el: val, d: dist};
+        return ent;
+    })
+
+    entries.sort((a, b) => a.d - b.d);
+    entries.forEach(entry => dropdown.appendChild(entry.el));
+
+    let counter: number = 0;
+    entries.forEach((entry) => {
+        // 0 is for jaro-winkler
+        if (counter < count && entry.d > 0) {
+            entry.el.removeAttribute('hidden');
+        } else {
+            entry.el.setAttribute('hidden', '');
+        }
+        counter++;
+    })
 }
